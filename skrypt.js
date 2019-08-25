dedd5054d4cb207f412f90937cd3b653b02dbfc8
Kopiec <46580390+Kopiec@users.noreply.github.com>
2019-08-25 02:59:55 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -1,8 +1,8 @@
 function przemianaR(){
     var tekst = document.getElementById('TuJestR');
     var outPut = document.getElementById('TuWychodziR');
-    var reg = tekst.value.replace(/r/g, "#");
-    var reg2 = reg.replace(/R/g, "@");
+    var reg = tekst.value.replace(/r/g, "ł");
+    var reg2 = reg.replace(/R/g, "Ł");
     outPut.value = reg2;
     tekst.placeholder=tekst.value;
     tekst.value="";
@@ -11,8 +11,8 @@ function przemianaR(){
 function odmianaR(){
     var text = document.getElementById('TuWychodziR');
     var wyjscie = document.getElementById('TuJestR');
-    var wyr = text.value.replace(/#/g, "r");
-    var wyr2 = wyr.replace(/@/g, "R");
+    var wyr = text.value.replace(/ł/g, "r");
+    var wyr2 = wyr.replace(/Ł/g, "R");
     wyjscie.value=wyr2;
     text.placeholder=text.value;
     text.value="";
