fddf94e7e1addbae170f39936f65d6df9b05a1c4
Kopiec <46580390+Kopiec@users.noreply.github.com>
2020-07-24 22:41:56 +0200

    test

--- counter.js
+++ counter.js
@@ -34,7 +34,7 @@ function counterTime(){
     }
     else{
         stopFunction();
-        return '<iframe width="1120" height="630" src="https://www.youtube.com/embed/QuqJOnO-mr8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        return '<iframe src="https://player.twitch.tv/?channel=wzitoldo&parent=www.example.com" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>';
     }
 }
 
