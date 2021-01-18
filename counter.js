8467f0c5321f57ced21e0d1873116e0fc3c7a7c8
Kopiec <46580390+Kopiec@users.noreply.github.com>
2021-01-18 19:53:27 +0100

    zapiski

---
+++ counter.js
@@ -0,0 +1,52 @@
+var handler = document.getElementById('counter');
+
+var eventTime = new Date(2021, 0, 21, 20, 00, 00);
+
+function stopFunction() {
+  clearInterval(inter);
+}
+
+function counterTime(){
+    var time = new Date();
+    
+    var remainedTime = eventTime.getTime() - time.getTime();
+    
+    if(remainedTime>0){
+        var s = remainedTime / 1000; //seconds
+        var m = s / 60;  //minutes
+        var h = m / 60; //hours
+        
+        var sLeft = Math.floor(s % 60);
+        var mLeft = Math.floor(m % 60);
+        var hLeft = Math.floor(h);
+        var D = hLeft / 24;
+        var Dleft= Math.floor(D);
+        
+        if(mLeft<10){
+            mLeft = "0" + mLeft;
+        }
+        if(sLeft<10){
+            sLeft = "0" + sLeft;
+        }
+        while(hLeft>=24){
+            hLeft=hLeft-24;
+        }
+
+        if(hLeft<10){
+            hLeft = "0" + hLeft;
+        }
+        
+        return Dleft+" Dni "+hLeft+":"+mLeft+":"+sLeft;
+    }
+    else{
+        stopFunction();
+        return "0 Dni 00:00:00";
+    }
+}
+
+var inter = setInterval("handler.innerHTML = counterTime()", 1000);
+
+window.onload = function(){
+    handler.innerHTML = counterTime();
+    inter;
+}
